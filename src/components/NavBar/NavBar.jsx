f0ee72451e015d0dec15ec119e22792dbb35eaa4
adi50920 <aaditya.rai@dmce.ac.in>
2024-01-06 10:39:18 +0530

    navbar updated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,43 +4,42 @@ import Logo from "../Logo/Logo";
 import SearchBar from "../SearchBar/SearchBar.jsx";
 import FeedBackModal from "../FeedBackModal/FeedBackModal";
 import { useNavigate } from "react-router-dom";
+
 const NavBar = ({ data, logo = false, search = false, feedback = false }) => {
   const [isFeedBackModalOpen, setIsFeedBackModalOpen] = useState(false);
   const navigate = useNavigate();
+
   const toggleFeedBackModal = (value = false) => {
     setIsFeedBackModalOpen(value);
   };
-  //   const _onSuccess = () => {
-  //     show toast
-  //     showToast("Feedback Submitted", "success");
-  //   };
+
   return (
     <div className={styles.wrapper}>
       <nav className={styles.navbarContainer}>
         <div className={styles.logoWrapper} onClick={() => navigate(`/`)}>
-          {logo ? <Logo id={styles.logo} /> : null}
+          {logo && <Logo id={styles.logo} />}
         </div>
-        {search ? (
+        {search && (
           <div className={styles.searchWrapper}>
             <SearchBar
-              placeholder="Search a album of your choice"
+              placeholder="Search an album of your choice"
               data={data}
             />
           </div>
-        ) : null}
-        {feedback ? (
+        )}
+        {feedback && (
           <div
             className={styles.nav_link}
             onClick={() => toggleFeedBackModal(true)}
           >
             Feedback
           </div>
-        ) : null}
+        )}
       </nav>
       <FeedBackModal
         isOpen={isFeedBackModalOpen}
-        //    onSuccess={_onSuccess}
-        onDismiss={toggleFeedBackModal}
+        // onSuccess={_onSuccess}
+        onDismiss={() => toggleFeedBackModal(false)}
       />
     </div>
   );
